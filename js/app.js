5be3847549e9cf5004d6a9cb96653aee750094e9
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-04-09 20:40:26 -0400

    finished question 1

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    let sumOfNum = a + b;
-    let message = `The sum of ${a} and ${b} is ${sumOfNum}.`
-    return [sumOfNum, message]
+  let sumOfNum = a + b;
+  let message = `The sum of ${a} and ${b} is ${sumOfNum}.`;
+  return [sumOfNum, message];
 }
 
 // Here is the test for sum(); uncomment it to run it
